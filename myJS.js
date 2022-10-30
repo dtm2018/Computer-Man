37c1001de806a492f48ed2fb3d8f0fc9b6148c87
DALTech Mobile <31376197+dtm2018@users.noreply.github.com>
2022-10-30 19:04:45 -0400

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -1,8 +1,10 @@
 function dtmWait1s() {
   const date = Date.now();
-  let miliSecs = 1000;
+  let miliSecs = 4000;
   let currentDate = 0;
-  alert();
+  
+  alert('dana');
+  
   do {
     currentDate = Date.now();
   } while (currentDate - date < miliSecs);
