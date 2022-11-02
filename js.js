38f9ec89c40911deb2327769914d43e6cccd1643
dtm2018 <dtmxxx2017@gmail.com>
2022-11-02 16:10:43 -0400

    my git 1

--- js.js
+++ js.js
@@ -1,9 +1,8 @@
-var sleep1 = (ms) => {
+var sleepSyn = (ms) => {
     const start = Date.now();
     let now = start;
     while (now - start < ms) {
       now = Date.now();
     }
 }
-sleep1(1777);
-//alert('3 secs');
+sleepSyn(1777);
